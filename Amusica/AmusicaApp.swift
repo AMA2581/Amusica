03e670beeae50afb700bc0176aedc560b219bbee
AMA2581 <amir.abravesh.ama@gmail.com>
2023-09-24 20:55:08 +0330

    Initial Commit

---
+++ Amusica/AmusicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmusicaApp.swift
+//  Amusica
+//
+//  Created by Amir Mahdi Abravesh on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct AmusicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
